1d1d5a78c581626c57746639b2065abc790dbcd7
Tonomatic <antonysoly2217@gmail.com>
2021-12-18 22:02:02 -0500

    first stack example

---
+++ DataStructures/Stack/examples.js
@@ -0,0 +1,30 @@
+
+// Valid Parentheses
+
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+
+var isValid = function(s) {
+    let stack = [];
+    let pairs = { '(': ')', '[': ']', '{': '}'}
+    for(let i = 0; i < s.length; i++) {
+        let char = s[i]; // {
+
+        if(pairs[char]) {
+            stack.push(char)
+        } else {
+            if(pairs[stack[stack.length-1]] !== char) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+    }
+
+    return true;
+}
