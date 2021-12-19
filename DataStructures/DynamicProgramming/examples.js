4b9d9aba401edc702646365d076c80bbd7b31eec
Tonomatic <antonysoly2217@gmail.com>
2021-12-19 18:18:54 -0500

    added common dynamic programming problem and array example.

---
+++ DataStructures/DynamicProgramming/examples.js
@@ -0,0 +1,15 @@
+
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+// A subarray is a contiguous part of an array.
+
+
+
+var maxSubArray = function(nums) {
+    let maxN = nums[0];
+    let currentN = 0;
+    for (let n of nums) {
+        currentN = currentN < 0 ? n : currentN + n;
+        maxN = Math.max(currentN, maxN);
+    }
+    return maxN;
+};
